0d78883632a9d1ad2aaf65232fc4be7799132c21
Gabriel Costa Silva <gabriel_costasilva@yahoo.com.br>
2023-07-07 22:15:58 -0300

    Cleaned unnecessary code

--- src/main/java/com/example/testcontainersb/CustomerRepository.java
+++ src/main/java/com/example/testcontainersb/CustomerRepository.java
@@ -1,7 +1,5 @@
 package com.example.testcontainersb;
 
 import org.springframework.data.repository.ListCrudRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource
 public interface CustomerRepository extends ListCrudRepository<Customer, Long> { }
