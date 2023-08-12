ac0876a6ff1310ce32df732ea33ff918eeb409a7
Gabriel Costa Silva <gabriel_costasilva@yahoo.com.br>
2023-08-12 11:50:47 -0300

    Added Spring 3 feature

--- src/test/java/com/example/testcontainersb/TestcontainerSbApplicationTests.java
+++ src/test/java/com/example/testcontainersb/TestcontainerSbApplicationTests.java
@@ -5,8 +5,7 @@ import static org.junit.Assert.assertEquals;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -19,15 +18,8 @@ class TestcontainerSbApplicationTests {
 	private CustomerRepository repo;
 
 	@Container
+	@ServiceConnection
 	private static MySQLContainer container = new MySQLContainer("mysql:8.0.26");
-
-	@DynamicPropertySource
-	public static void overrideProps(DynamicPropertyRegistry registry) {
-		registry.add("spring.datasource.url", container::getJdbcUrl);
-		registry.add("spring.datasource.username", container::getUsername);
-		registry.add("spring.datasource.password", container::getPassword);
-		registry.add("spring.datasource.password", container::getPassword);
-	}
 
 	@Test
 	void test() {
