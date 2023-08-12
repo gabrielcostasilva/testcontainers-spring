94ac20395d7bbc907c660e72f377f35da72f2b26
Gabriel Costa Silva <gabriel_costasilva@yahoo.com.br>
2023-08-12 12:21:00 -0300

    Added test-main branch

--- src/main/java/com/example/testcontainersb/TestcontainerSbApplication.java
+++ src/main/java/com/example/testcontainersb/TestcontainerSbApplication.java
@@ -1,7 +1,12 @@
 package com.example.testcontainersb;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.MySQLContainer;
 
 @SpringBootApplication
 public class TestcontainerSbApplication {
@@ -10,4 +15,21 @@ public class TestcontainerSbApplication {
 		SpringApplication.run(TestcontainerSbApplication.class, args);
 	}
 
+	@Bean
+	public ApplicationRunner init(CustomerRepository repo) {
+		return args -> System.out.println("** Repo size: " + repo.count() + " ** ");
+
+	}
+
+	@TestConfiguration(proxyBeanMethods = false)
+	class MyContainersConfiguration {
+
+		@Bean
+		@ServiceConnection
+		MySQLContainer container() {
+			return new MySQLContainer("mysql:8.0.26");
+
+		}
+
+	}
 }
--- src/test/java/com/example/testcontainersb/TestcontainerSbApplicationTests.java
+++ src/test/java/com/example/testcontainersb/TestcontainerSbApplicationTests.java
@@ -4,26 +4,23 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
+import com.example.testcontainersb.TestcontainerSbApplication.MyContainersConfiguration;
+
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
-@Testcontainers
 class TestcontainerSbApplicationTests {
 
-	@Autowired
-	private CustomerRepository repo;
-
-	@Container
-	@ServiceConnection
-	private static MySQLContainer container = new MySQLContainer("mysql:8.0.26");
-
-	@Test
-	void test() {
-		assertEquals(0, repo.findAll().size());
-	}
+	public static void main(String[] args) {
+        SpringApplication
+			.from(TestcontainerSbApplication::main)
+			.with(MyContainersConfiguration.class)
+			.run(args);
+    }
 
 }
