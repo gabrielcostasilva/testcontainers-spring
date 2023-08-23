ae4d9c398225a28e1460bef2cfb1257f6a77e4d0
Gabriel Costa Silva <gabriel_costasilva@yahoo.com.br>
2023-08-23 20:23:34 -0300

    Added selenide-selenium-grid project

--- src/main/java/com/example/testcontainersb/CustomerRepository.java
+++
@@ -1,5 +0,0 @@
-package com.example.testcontainersb;
-
-import org.springframework.data.repository.ListCrudRepository;
-
-public interface CustomerRepository extends ListCrudRepository<Customer, Long> { }
--- src/test/java/com/example/testcontainersb/TestcontainerSbApplicationTests.java
+++ src/test/java/com/example/testcontainersb/TestcontainerSbApplicationTests.java
@@ -1,29 +1,58 @@
 package com.example.testcontainersb;
 
-import static org.junit.Assert.assertEquals;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.containers.BrowserWebDriverContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+
+import com.codeborne.selenide.WebDriverRunner;
+
+import static com.codeborne.selenide.Condition.exist;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byValue;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.testcontainers.containers.BrowserWebDriverContainer.VncRecordingMode.RECORD_ALL;
+
+import java.nio.file.Paths;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 @Testcontainers
 class TestcontainerSbApplicationTests {
 
-	@Autowired
-	private CustomerRepository repo;
-
 	@Container
-	@ServiceConnection
-	private static MySQLContainer container = new MySQLContainer("mysql:8.0.26");
+	private static BrowserWebDriverContainer container = new BrowserWebDriverContainer<>()
+			.withRecordingMode(
+					RECORD_ALL,
+					Paths.get(".", "target").toFile())
+			.withCapabilities(new FirefoxOptions());
 
 	@Test
 	void test() {
-		assertEquals(0, repo.findAll().size());
+		RemoteWebDriver remote = new RemoteWebDriver(
+				container.getSeleniumAddress(),
+				new FirefoxOptions());
+
+		WebDriverRunner.setWebDriver(remote);
+
+		open("https://google.com?q=selenide");
+
+		$$("form")
+				.filterBy(exist)
+				.first()
+				.shouldBe(visible)
+				.submit();
+
+		$(byValue("selenide"))
+				.shouldBe(exist)
+				.shouldBe(visible)
+				.submit();
+
 	}
 
 }
