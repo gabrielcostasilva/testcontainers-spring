6a3a45b7823c036f56fa857c9e292959ae172665
Gabriel Costa Silva <gabriel_costasilva@yahoo.com.br>
2023-08-23 21:28:16 -0300

    Added sql-schema project

---
+++ src/main/resources/testcontainers-schema.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE  IF NOT EXISTS `test` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `test`;
+
+CREATE TABLE `customer` (
+  `id` varchar(255) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
--- src/test/java/com/example/testcontainersb/TestcontainerSbApplicationTests.java
+++ src/test/java/com/example/testcontainersb/TestcontainerSbApplicationTests.java
@@ -6,12 +6,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.test.context.jdbc.Sql;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 @Testcontainers
+@Sql("/testcontainers-schema.sql")
 class TestcontainerSbApplicationTests {
 
 	@Autowired
